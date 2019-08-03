8ce3895bb3a6dce0c8d42e723eca095e1a5cfeae
Pradeep B <pradeep87blore@users.noreply.github.com>
2019-08-04 00:19:52 +0530

    AWS Load Balancer test util added

---
+++ AWS_Utils/AWSLoadBalancerLoadTester/AWSLoadBalancerLoadTester/LoadTester.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace AWSLoadBalancerLoadTester
+{
+    public class LoadTester
+    {
+        string URL = null;
+        int max_reads = 10;
+        int max_threads = 10;
+
+        public LoadTester(string url, int maxReads, int maxThreads)
+        {
+            URL = url;
+            max_reads = maxReads;
+            max_threads = maxThreads;
+        }
+
+        public void StartLoadTest()
+        {
+            for (int iIndex = 0; iIndex < max_threads; iIndex++)
+            {
+                Thread newThread = new Thread(AddLoad);
+                newThread.Start();
+            }
+        }
+
+        private void AddLoad(object obj)
+        {            
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (int iIndex = 0; iIndex < max_reads; iIndex++)
+            {
+                Console.WriteLine(string.Format("Thread ID: {0}, Iteration: {1}, Output: {2}", thread_id.ToString(), iIndex, Get(URL)));
+            }            
+        }
+
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ AWS_Utils/AWSLoadBalancerLoadTester/AWSLoadBalancerLoadTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AWSLoadBalancerLoadTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Load Test");
+
+            LoadTester loadTester = new LoadTester("http://alb-1646804891.us-east-1.elb.amazonaws.com/", 10, 20);
+            loadTester.StartLoadTest();
+        }
+    }
+}
